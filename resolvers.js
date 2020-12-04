10d1c3bd2613d2f1f59519f19d1c896acfa57360
NahashonM <nahashonmwongera@hotmail.com>
2020-12-04 14:03:08 +0300

    schema modified to more specialized requests

--- resolvers.js
+++ resolvers.js
@@ -2,21 +2,53 @@
 
 const resolvers = {
 	Query: {
+		user: (root, {user_id, password}) => {
+			// Todo--
+			// Query db
+
+		},
+		
 		locations: () => {
-			return [ {no_of_visits: 1}, {no_of_visits: 2}];
+			// Todo--
+			// Query db
 		},
 
-		location: (root, {id}) => {
-			if( id == 1)
-				return {no_of_visits: 1};
-			else
-				return {};
+		key_issues: () => {
+			// Todo--
+			// Query db
 		},
+
+		location_staff: (root, {location_id}) => {
+			// Todo--
+			// Query db
+		},
+
+		graph_values: (root, {location_id, period}) => {
+			// Todo--
+			// Query db
+		}
 	},
 
+
 	Mutation: {
-		AddAssessment:() => {
-			
+		UpdateStaffAssessment:(root, {
+										staff_no,
+										efficiency,
+										e_delta,
+										nps,
+										nps_delta,
+										efficiency_percent }) => {
+			// ToDo----
+			// Add to db
+		},
+
+		AddIssue:(root, {
+							location_id,
+							reporting_staff_no,
+							description,
+							isKeyIssue}) => {
+			// ToDo----
+			// Add to db
 		}
 	}
   };
--- typedefs.js
+++ typedefs.js
@@ -2,72 +2,95 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 type Query {
-	user(id: Int!, password: String!): User
+
+	user(user_id: Int!, password: String!): User
+
 
 	locations: [ Location ]
-	location(id: Int!): Location
+
+	key_issues: [ Key_Issue ]
+
+	location_staff(location_id: Int!): [ Staff ]
+
+	graph_values(location_id: Int!, period: String): [ Graph_Values ]
+
 }
 
 
-type User {
-	id:						Int
-	name:					String!
-	email:					String!
-	no_of_patients_seen:	Int
-}
+	type User {
+		id:						Int!
+		name:					String!
+		email:					String
+		no_of_patients_seen:	Int
+		password_hash:			String
+	}
+
+	type Location {
+		id:				Int!
+		name:			String!
+		no_of_visits:	Int
+	}
 
 
-type Location {
-	id:						Int
-	name:					String!
-	no_of_visits:			Int
-	staff:					[ Staff! ]
-	revenue:				Revenue
-	foot_fall:				Foot_fall
-	partient_satisfaction:	Partient_satisfaction
-}
+	type Staff {
+		staff_no:				Int!
+		staff_name:				String!
+		efficiency:				Int
+		efficiency_delta:		Float
+		nps:					Float
+		nps_delta:				Float
+		efficiency_percent:		Float
+		no_of_issues_reported:	Int
+	}
+
+	type Graph_Values {
+		foot_fall_graph: [Foot_Fall]
+		patient_sartisfaction_graph: [Partient_Sartisfaction]
+		revenue_graph: [Revenue]
+	}
 
 
-type Staff {
-	staff_no:			Int!
-	staff_name:			String!
-	efficiency:			Int
-	e_delta:			Float
-	nps:				Float
-	nps_delta:			Float
-	efficiency_percent:	Float
-	reported_issues:	[ Issues ]
-}
+	type Foot_Fall {
+		patient_count:	Float
+		patient_cdelta:	Float
+		graph_values:	[ Float ]
+	}
 
+	type Partient_Sartisfaction {
+		nps:			Float
+		nps_delta:		Float
+		graph_values:	[ Float ]
+	}
 
-type Foot_fall {
-	patient_count:	Float
-	patient_cdelta:	Float
-	graph_values:	[ Float ]
-}
+	type Revenue {
+		revenue:		Float
+		revenue_delta:	Float
+		graph_values:	[ Float ]
+	}
 
-type Partient_satisfaction {
-	nps:			Float
-	nps_delta:		Float
-	graph_values:	[ Float ]
-}
-
-type Revenue {
-	revenue:		Float
-	revenue_delta:	Float
-	graph_values:	[ Float ]
-}
-
-type Issues {
-	description:	String!
-	is_key_issue:	Boolean!
-}
+	type Key_Issue {
+		description:	String!
+		location_name:	String!
+	}
 
 
 type Mutation {
-	AddAssessment:String
- }
+	UpdateStaffAssessment(
+					staff_no:			Int!,
+					efficiency:			Int,
+					e_delta:			Float,
+					nps:				Float,
+					nps_delta:			Float,
+					efficiency_percent:	Float
+		):String
 
+	AddIssue (
+			location_id:		Int!,
+			reporting_staff_no: Int!,
+			description:		String!,
+			isKeyIssue:			Boolean
+		): String
+ }
 
 `;
 
